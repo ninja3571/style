09cd438a64cf04a4b8ed8756918233b73c28d731
ninja3571 <125036855+ninja3571@users.noreply.github.com>
2024-07-09 23:36:54 +0200

    1.3

---
+++ app.js
@@ -0,0 +1,14 @@
+function punkty(){
+    for(var i=1; i<=10; i++){
+        var kropek = document.createElement('div')
+        kropek.classList.add('falaa')
+        kropek.style.position='absolute'
+
+        kropek.style.top='12.5%'
+        kropek.style.left=36.5+2*i+"%"
+
+        kropek.style.animation='falanga 1.5s '+ i/10 +'s infinite';
+
+        document.getElementById("fala").appendChild(kropek)
+    }
+}

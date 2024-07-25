400eb7091337eb14074f7a409a02859667ea609b
ninja3571 <125036855+ninja3571@users.noreply.github.com>
2024-07-26 00:24:20 +0200

    1.6

--- app.js
+++ app.js
@@ -12,3 +12,37 @@ function punkty(){
         document.getElementById("fala").appendChild(kropek)
     }
 }
+
+function gora(){
+    var x = document.getElementById('xx').value
+    var y = document.getElementById('yy').value
+    var z = document.getElementById('zz').value
+    var angle = document.getElementById('angle').value
+    var top = document.getElementById("top").value
+    var left = document.getElementById("left").value
+
+    var gora=document.getElementById("b6")
+    if(x=="") {
+        x=0
+    }
+    if(y=="") {
+        y=0
+    }
+    if(z=="") {
+        z=0
+    }
+    if(angle=="") {
+        angle=0
+    }
+
+    if(top!=""){
+        gora.style.top=top+"vh"
+    }
+    if(left!=""){
+        gora.style.left=left+'%'
+    }
+
+    gora.style.background='radial-gradient(rgb(252, 172, 0), rgb(163, 122, 9))'
+    gora.style.transform='rotate3d('+x+', '+y+', '+z+', '+angle+'deg'+')'
+    console.log(x,y,z,angle,'deg')
+}
